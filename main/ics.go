1959dca936aba95589543ce8cf16b912bc11854a
Dumitru Paul-Cristian <jamez4paul@users.noreply.github.com>
2017-11-13 16:11:36 +0200

    Initial commit

---
+++ main/ics.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "ics"
+  type: "sprite"
+  data: "tile_set: \"/main/Atlasuri/ics.atlas\"\n"
+  "default_animation: \"x\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 1.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
